e070de3179b03fbafafe1ce890af621f9409e047
SGFgab <gabrielsilvacarva@gmail.com>
2026-05-30 20:09:09 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eletropong-v2';
+const CACHE_NAME = 'eletropong-v3'; // Já coloquei v3 aqui para forçar a atualização!
 const ASSETS = [
   './',
   './index.html',
@@ -6,7 +6,6 @@ const ASSETS = [
   'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@400;500;600;700&display=swap'
 ];
 
-// Instala o Service Worker e guarda os arquivos essenciais no cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -15,7 +14,6 @@ self.addEventListener('install', (e) => {
   );
 });
 
-// Ativa e limpa caches antigos se houver atualização
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
@@ -30,21 +28,10 @@ self.addEventListener('activate', (e) => {
   );
 });
 
-// Serve os arquivos direto do cache para máxima velocidade
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((cachedResponse) => {
       return cachedResponse || fetch(e.request);
     })
-  );
-});self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('pingpong-v1').then((cache) => cache.addAll(['./', './index.html']))
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
