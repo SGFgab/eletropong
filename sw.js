ac93f6dfa47f61a3b2ae3a3e727837d3917c9aa5
SGFgab <gabrielsilvacarva@gmail.com>
2026-05-30 19:08:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,43 @@
+const CACHE_NAME = 'eletropong-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@400;500;600;700&display=swap'
+];
+
+// Instala o Service Worker e guarda os arquivos essenciais no cache
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativa e limpa caches antigos se houver atualização
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Serve os arquivos direto do cache para máxima velocidade
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('pingpong-v1').then((cache) => cache.addAll(['./', './index.html']))
   );
